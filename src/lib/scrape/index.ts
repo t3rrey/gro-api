da16455e0324abf1219674b620e989e3a1be13ef
Lachlan <lachlan@Lachlans-MacBook-Pro.local>
2023-06-16 17:23:53 +0200

    feat: finalise basic scraping

--- src/lib/scrape/index.ts
+++ src/lib/scrape/index.ts
@@ -1,18 +1,18 @@
 import puppeteer from "puppeteer";
+import { convertKjToKcal } from "../util";
 
 interface NutritionFacts {
-  protein: string[];
-  totalFat: string[];
-  saturatedFat: string[];
-  carbohydrate: string[];
-  sugars: string[];
-  dietaryFibre: string[];
-  sodium: string[];
-  calories: string[];
-  [key: string]: string[];
+  calories: number;
+  protein: number;
+  totalFat: number;
+  saturatedFat: number;
+  carbohydrate: number;
+  sugars: number;
+  dietaryFibre: number;
+  sodium: number;
 }
 
-export const scrapePage = async (url: string): Promise<NutritionFacts> => {
+export const scrapePage = async (url: string): Promise<NutritionFacts[]> => {
   const browser = await puppeteer.launch({
     defaultViewport: null,
   });
@@ -21,9 +21,8 @@ export const scrapePage = async (url: string): Promise<NutritionFacts> => {
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"
   );
   await page.goto(url);
-  let test;
 
-  const nutritionFacts: NutritionFacts = await page.evaluate(() => {
+  const nutritionFacts: NutritionFacts[] = await page.evaluate(() => {
     let nutritionTable = document.querySelector(".nutrition-table");
     let nutritionRows = nutritionTable?.querySelectorAll(".nutrition-row");
     // Mapping table names to property names
@@ -37,39 +36,50 @@ export const scrapePage = async (url: string): Promise<NutritionFacts> => {
       dietaryfibre: "dietaryFibre",
       sodium: "sodium",
     };
-
-    let facts: NutritionFacts = {
-      calories: [],
-      protein: [],
-      totalFat: [],
-      saturatedFat: [],
-      carbohydrate: [],
-      sugars: [],
-      dietaryFibre: [],
-      sodium: [],
+    let factsPerServing: NutritionFacts = {
+      calories: 0,
+      protein: 0,
+      totalFat: 0,
+      saturatedFat: 0,
+      carbohydrate: 0,
+      sugars: 0,
+      dietaryFibre: 0,
+      sodium: 0,
     };
 
+    let factsPer100g: NutritionFacts = {
+      calories: 0,
+      protein: 0,
+      totalFat: 0,
+      saturatedFat: 0,
+      carbohydrate: 0,
+      sugars: 0,
+      dietaryFibre: 0,
+      sodium: 0,
+    };
     nutritionRows?.forEach((row) => {
       let columns = row.querySelectorAll(".nutrition-column");
-      let nutrient = columns[0]?.textContent
+      let nutrient = columns[0].textContent
         ?.trim()
         .toLowerCase()
-        .replace(" ", "")
+        .replace(/ /g, "")
         .replace(",", "");
+      let perServing = parseFloat(columns[1].textContent?.trim() || "0");
+      let per100g = parseFloat(columns[2].textContent?.trim() || "0");
 
-      let perServing = columns[1]?.textContent?.trim();
-      let per100g = columns[2]?.textContent?.trim();
-      // Check if the nutrient exists in the mapping and then update the facts object
       if (nutrient && nameMapping.hasOwnProperty(nutrient)) {
-        facts[nameMapping[nutrient]] = [perServing || "", per100g || ""];
+        factsPerServing[nameMapping[nutrient] as keyof NutritionFacts] =
+          perServing;
+        factsPer100g[nameMapping[nutrient] as keyof NutritionFacts] = per100g;
       }
     });
 
-    return facts;
+    return [factsPerServing, factsPer100g];
   });
-
   await browser.close();
-  console.log("nutrition facts", nutritionFacts);
+  nutritionFacts.forEach((facts) => {
+    facts.calories = convertKjToKcal(facts.calories);
+  });
   console.log(nutritionFacts);
   return nutritionFacts;
 };
